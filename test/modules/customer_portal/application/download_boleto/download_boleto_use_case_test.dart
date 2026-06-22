1616839154d211ae8720f7b858ce181e098112ba
Gustavo Teixeira <gteixeiradesousa7@gmail.com>
2026-06-22 19:40:41 +0000

    ✨ feat(customer_portal): adicionar repositório e caso de uso para boletos

---
+++ test/modules/customer_portal/application/download_boleto/download_boleto_use_case_test.dart
@@ -0,0 +1,33 @@
+import 'package:ecommerce_b2b/modules/customer_portal/application/download_boleto/download_boleto_use_case.dart';
+import 'package:ecommerce_b2b/modules/customer_portal/domain/repositories/boleto_repository.dart';
+import 'package:ecommerce_b2b/modules/customer_portal/domain/value_objects/boleto_copy.dart';
+import 'package:ecommerce_b2b/modules/shared_kernel/domain/common/ids/order_id.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class MockBoletoRepository implements BoletoRepository {
+  @override
+  Future<BoletoCopy> getBoletoByOrder(OrderId orderId) async {
+    return const BoletoCopy(
+      barcode: '123456789',
+      url: 'https://boleto.com/123',
+    );
+  }
+}
+
+void main() {
+  late DownloadBoletoUseCase useCase;
+  late MockBoletoRepository repository;
+
+  setUp(() {
+    repository = MockBoletoRepository();
+    useCase = DownloadBoletoUseCase(repository);
+  });
+
+  test('should return boleto data for a given order', () async {
+    const orderId = OrderId('o1');
+    final result = await useCase.execute(orderId);
+
+    expect(result.barcode, '123456789');
+    expect(result.url, 'https://boleto.com/123');
+  });
+}
---
+++ test/modules/customer_portal/application/get_purchase_history/get_purchase_history_use_case_test.dart
@@ -0,0 +1,40 @@
+import 'package:ecommerce_b2b/modules/customer_portal/application/get_purchase_history/get_purchase_history_use_case.dart';
+import 'package:ecommerce_b2b/modules/order_flow/domain/aggregates/sales_order/sales_order.dart';
+import 'package:ecommerce_b2b/modules/order_flow/domain/enums/credit_status.dart';
+import 'package:ecommerce_b2b/modules/order_flow/domain/enums/order_status.dart';
+import 'package:ecommerce_b2b/modules/order_flow/domain/repositories/sales_order_repository.dart';
+import 'package:ecommerce_b2b/modules/shared_kernel/domain/common/ids/company_id.dart';
+import 'package:ecommerce_b2b/modules/shared_kernel/domain/common/ids/order_id.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class MockSalesOrderRepository implements SalesOrderRepository {
+  @override
+  Future<List<SalesOrder>> findByCompanyId(CompanyId companyId) async {
+    return [
+      SalesOrder(
+        id: const OrderId('o1'),
+        status: OrderStatus.delivered,
+        creditStatus: CreditStatus.approved,
+        items: [],
+      ),
+    ];
+  }
+}
+
+void main() {
+  late GetPurchaseHistoryUseCase useCase;
+  late MockSalesOrderRepository repository;
+
+  setUp(() {
+    repository = MockSalesOrderRepository();
+    useCase = GetPurchaseHistoryUseCase(repository);
+  });
+
+  test('should return purchase history for a given company', () async {
+    const companyId = CompanyId('c1');
+    final result = await useCase.execute(companyId);
+
+    expect(result, isNotEmpty);
+    expect(result.first.id, const OrderId('o1'));
+  });
+}
